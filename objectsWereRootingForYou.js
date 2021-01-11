0bf12ab8e32ac862fc786e3aa137a1f2c943cd04
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-01-11 06:10:12 +0000

    new code for assignment Objects Were Rooting For You

--- objectsWereRootingForYou.js
+++ objectsWereRootingForYou.js
@@ -21,7 +21,7 @@ const metric = 'redness'
 const judgeVegetable = function (vegetables, metric) {
 
 let sortedVegetables = [];
-/*
+
   //iterates over the array of objects to see if the metric argument is valid
   for (let i = 0; i < vegetables.length; i++){
       if (vegetables[i][metric] === undefined) {
@@ -29,25 +29,6 @@ let sortedVegetables = [];
     }
   }
 
-  let scores = []; //an array for storing an array with values for the scores and the index of it's object in the vegetables array
-
-  vegetables.forEach(function(vegetable, index){
-    scores.push([vegetable[metric],index]);
-  });
-  
-  // sort the array of arrays by the index that contains the score only, descending sort
-  scores = scores.sort(function(a,b){
-    if (a[0] > b[0] ){
-      return -1;
-    } else if (a[0] === b[0]){
-      return 0;
-    } else {
-      return 1;
-    }
-  });
-  
-  return vegetables[scores[0][1]].submitter; //return the submitter property of the object with the highest scoring metric
-*/
 sortedVegetables = vegetables.sort(function(a,b){
   if (a[metric] > b[metric] ){
     return -1;
